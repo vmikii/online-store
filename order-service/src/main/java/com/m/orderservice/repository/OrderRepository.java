2c42a8aa40acb80fe28660e19dd315e251b31a1e
Miki <veghmiki@gmail.com>
2023-03-07 13:15:29 +0100

    add repo: OrderRepository

---
+++ order-service/src/main/java/com/m/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.m.orderservice.repository;
+
+import com.m.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
