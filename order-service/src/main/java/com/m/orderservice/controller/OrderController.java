56594f216f504cef1e82be7dcd143791b79d693a
Miki <veghmiki@gmail.com>
2023-03-07 13:16:09 +0100

    add controller: OrderController

---
+++ order-service/src/main/java/com/m/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.m.orderservice.controller;
+
+import com.m.orderservice.dto.OrderRequest;
+import com.m.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createOrder(@RequestBody OrderRequest orderRequest){
+        orderService.createOrder(orderRequest);
+        return "Order created successfully.";
+    }
+}
